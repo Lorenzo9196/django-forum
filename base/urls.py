7837775d2af8da129d6d04a468b0887f7094e0bf
lorenzo <lorenzo9196@hotmail.com>
2023-06-03 22:48:59 -0500

    Crud for Room

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('room/<int:pk>/', views.room, name='room'),
+    path('create-room/', views.createRoom, name='create-room'),
+    path('update-room/', views.updateRoom, name='update-room'),
+    path('delete-room/<int:pk>/', views.deleteRoom, name='delete-room'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 def home(request):
@@ -10,4 +11,31 @@ def home(request):
 def room(request,pk):
      room = Room.objects.get(id=pk)
      return render(request, 'room.html', {'room': room})
+
+def createRoom(request):
+     form = RoomForm()
+     if request.method == 'POST':
+          form = RoomForm(request.POST)
+          if form.is_valid():
+              form.save()
+              return redirect('home')
+     return render(request, 'create_room.html', {'form': form})
+
+def updateRoom(request,pk):
+     room = Room.objects.get(id=pk)
+     form = RoomForm(request.POST, instance=room)
+     if request.method == 'POST':
+          form = RoomForm(request.POST, instance=room)
+          if form.is_valid():
+              form.save(request.POST, instance=room)
+              return redirect('home')
+     return render(request, 'update_room.html', {'form': form})
+
+def deleteRoom(request,pk):
+     room = Room.objects.get(id=pk)
+     if request.method == 'POST':
+          room.delete()
+          return redirect('home')
+     return render(request, 'delete_room.html', {'room': room})
+
 
